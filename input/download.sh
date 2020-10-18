bf88b3fcc3bc27bb13d3a9a961533db508f9f820
seiichiro0427 <n.s.kkro@gmail.com>
2020-10-18 21:19:17 +0900

    update

---
+++ input/download.sh
@@ -0,0 +1,3 @@
+kaggle competitions download -c rsna-str-pulmonary-embolism-detection -f train.csv
+kaggle competitions download -c rsna-str-pulmonary-embolism-detection -f test.csv
+kaggle competitions download -c rsna-str-pulmonary-embolism-detection -f sample_submission.csv
